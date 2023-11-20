a3d22e26da2f358a0d98efe87a8b6a1502eacfa1
Ricardo Fernandes <143021198+ricardolagarini@users.noreply.github.com>
2023-11-20 01:13:45 +0000

    l
    ç

--- Java_pratica/src/Proj_pratico/Biblioteca_proj.java
+++ Java_pratica/src/Proj_pratico/Biblioteca_proj.java
@@ -2,6 +2,7 @@ package Proj_pratico;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class Biblioteca_proj {
@@ -13,7 +14,6 @@ public class Biblioteca_proj {
      * @throws FileNotFoundException Caso o ficheiro não seja encontrado
      */
     public static int contarLinhasFicheiro(String caminhoFicheiro) throws FileNotFoundException {
-
         File ficheiro = new File(caminhoFicheiro);
         Scanner scanner = new Scanner(ficheiro);
         int contagemLinhas = 0;
@@ -32,7 +32,6 @@ public class Biblioteca_proj {
      * @throws FileNotFoundException Caso o ficheiro não seja encontrado
      */
     public static int contarColunasFicheiro(String caminhoFicheiro, String delimitador) throws FileNotFoundException {
-
         File ficheiro = new File(caminhoFicheiro);
         Scanner scanner = new Scanner(ficheiro);
         int contagemColunas = 0;
@@ -62,10 +61,8 @@ public class Biblioteca_proj {
 
         // Declarar uma matriz com o tamanho adequado
         String[][] matrizTotal = new String[numeroLinhas][numeroColunas];
-
         String linhaAtual = scanner.nextLine();
         int linhaMatriz = 0;
-
         while (scanner.hasNextLine()) {
             linhaAtual = scanner.nextLine();
             String[] itensLinha = linhaAtual.split(";");
@@ -83,18 +80,23 @@ public class Biblioteca_proj {
      * @param matrizTotal Matriz a pesquisar
      */
     public static void listarJogosFicheiro(String[][] matrizTotal) {
-        boolean confirmaJogo = true;
-
+        int cont=0;
         for (int linha = 0; linha < matrizTotal.length; linha++) {
-            for (int coluna = 0; coluna < matrizTotal.length; coluna++) {
-                if (linha != coluna && matrizTotal[linha][7].equals(matrizTotal[coluna][7])) {
-                    confirmaJogo = false;
+
+            // Verifica as colunas até a linha atual para checar se ha jogos repetidos
+            for (int coluna = 0; coluna < linha; coluna++) {
+
+                // Verifica se o jogo na linha atual é igual ao jogo na linha anterior e se ainda não foi listado
+                if (linha != coluna && matrizTotal[linha][7].equals(matrizTotal[coluna][7])&&cont==0) {
+                   cont++; // Contador +1 se econtrar um jogo repetido
                 }
             }
-            if (confirmaJogo) {
+            // Se o jogo ainda nao foi listado, imprime nome do jogo
+            if(cont==0) {
                 System.out.println("Jogo: " + matrizTotal[linha][7]);
-            } else {
-                confirmaJogo = true;
+            }
+            else {
+            cont=0; // Reinicia o contador para a próx loop caso encntre jogo repetido
             }
         }
     }
@@ -115,7 +117,6 @@ public class Biblioteca_proj {
         }
     }
 
-
     /**
      * Registo de cliente
      *
@@ -123,7 +124,6 @@ public class Biblioteca_proj {
      */
     public static void registoCliente(Scanner input) {
         String nomeUsuario, emailUsuario, contactoUsuario;
-
         System.out.print("Insira seu nome: ");
         input.nextLine(); // limpar campo
         nomeUsuario = input.nextLine();
@@ -136,11 +136,136 @@ public class Biblioteca_proj {
     }
 
 
+    /**
+     * Método para imprimir na consola o conteudo de um ficheiro
+     * @param caminho Caminho do Ficheiro
+     * @throws FileNotFoundException Se não encontrar o ficheiro no caminho especificado
+     */
+    public static void imprimirFicheiro(String caminho) throws FileNotFoundException {
+        // Instanciar o Scanner para um ficheiro com o caminho recebido como parâmetro
+        Scanner fileScanner = new Scanner(new File(caminho));
+        String linha;
+        while (fileScanner.hasNextLine()) {
+            linha = fileScanner.nextLine();
+            linha = linha.replace(";", " | "); // Substituir ";" por "|"
+            System.out.println(linha);
+        }
+    }
+
+
+    /**
+     * Método para listar iDS clientes do ficheiro
+     *
+     * @param matrizTotal Matriz a pesquisar
+     */
+    public static void idClientesFicheiro(String[][] matrizTotal) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Digite o ID de cliente a ser pesquisado (1-60): ");
+
+        int cont=0;
+        String clienteInf = input.nextLine();
+        for (int linha = 0; linha < matrizTotal.length; linha++) {
+
+            // Verifica se o ID do cliente na linha atual é igual ao ID fornecido pelo usuario
+            if (matrizTotal[linha][1].equals(clienteInf)&&cont==0) {
+                cont++; // Contador +1 para evitar repetiçao do ID
+                System.out.println("ID: "+matrizTotal[linha][1] +" | "+ "Nome: "+matrizTotal[linha][2] +" | "+ "Telefone: "+matrizTotal[linha][3] +" | "+"Email: " +matrizTotal[linha][4]);
+            }
+        }
+    }
 
 
 
+    /**
+     * Método para listar quantas vendas foram executadas e o seu valor total
+     *
+     * @param matrizTotal Matriz a pesquisar
+     */
+    public static void regVendasTot(String[][] matrizTotal) {
+        double valorTotal = 0;
+        int quantVendas = 0; // Qtde vendas iniciado a zer
+        for (int linha = 0; linha < matrizTotal.length; linha++) {
+            double valor = Double.parseDouble(matrizTotal[linha][8]);
+            valorTotal += valor;
+            quantVendas++; // quando terminar de somar a ultima linha de valor, tem se a qtd de vendas = qtd de linhas do ficheiro
+        }
+        System.out.println("Número de vendas: " + quantVendas);
+        System.out.println("Valor total das vendas: " + valorTotal + " €");
+    }
 
+
+    /**
+     * Método para listar valor total de vendas + 20%
+     *
+     * @param matrizTotal Matriz a pesquisar
+     */
+    public static void VendasTotLucro(String[][] matrizTotal) {
+        double valorTotal = 0;
+        for (int linha = 0; linha < matrizTotal.length; linha++) {
+            double valor = Double.parseDouble(matrizTotal[linha][8]);
+            valorTotal += valor;
+        }
+        double lucroTotal=((valorTotal)*0.2)+((valorTotal));
+        System.out.println("Valor total do lucro de 20%: " + (valorTotal)*0.2 + " €");
+        System.out.println("Valor total do lucro de 20% + vendas totais: " + lucroTotal + " €");
+    }
+
+
+    /**
+     * Método para listar jogo mais caro e os clientes que o compraram
+     *
+     * @param matrizTotal Matriz a pesquisar
+     */
+    public static void jogoMaisCaro(String[][] matrizTotal) {
+        double precoMaior = 0;
+        String jogoMaior = "", clienteJogoMaior="";
+        int linha = 0;
+
+        while (linha < matrizTotal.length) {
+            double precoJogo = Double.parseDouble(matrizTotal[linha][8]);
+
+            // Verifica se o preço do jogo atual é maior que o preço do jogo mais caro registrado até o momento
+            if (precoJogo > precoMaior) {
+                precoMaior = precoJogo; // Atualiza o preço mais caro
+                jogoMaior = matrizTotal[linha][7]; // Atualiza o nome do jogo mais caro
+                clienteJogoMaior = matrizTotal[linha][2]; // guardar o primeiro cliente do jogoMaior
+
+                // Se encontrar um jogo com o mesmo preço que o jogo mais caro, entao =clientes que comprarar o jogo
+            } else if (precoJogo == precoMaior && jogoMaior.equals(matrizTotal[linha][7])) {
+                clienteJogoMaior = clienteJogoMaior + ", " + matrizTotal[linha][2];
+            }
+            linha++;
+        }
+        System.out.println("\nJogo mais caro é o " + jogoMaior +" e custa " +precoMaior+ " €");
+        System.out.println("Clientes que compraram esse jogo: " + clienteJogoMaior);
+    }
+
+
+
+    /**
+     * Método para listar jogos por categoria do ficheiro e imprmir esteticamente
+     *
+     * @param matrizTotal Matriz a pesquisar
+     */
+    public static void jogosPorcategoria(String[][] matrizTotal) {
+            Scanner input = new Scanner(System.in);
+            System.out.print("Editora a pesquisar: ");
+            String editora = input.nextLine();
+
+            for (int linha = 0; linha < matrizTotal.length; linha++) {
+                if (matrizTotal[linha][5].equals(editora)) {
+                    System.out.println("Categoria: " + matrizTotal[linha][6] +  " - " + matrizTotal[linha][7]);
+                }
+            }
+    }
 
 
 }
+
+
+
+
+
+
+
 
--- Java_pratica/src/Proj_pratico/proj_01.java
+++ Java_pratica/src/Proj_pratico/proj_01.java
@@ -10,19 +10,26 @@ public class proj_01 {
 
         String[][] matrizTotal = lerFicheiroParaMatriz("Ficheiros/GameStart_V2.csv");
 
+        //instanciar variaveis
         Scanner input = new Scanner(System.in);
-        int opcaoCliente=0 ;
+        int opcaoCliente=0, opcaoAdmin=0 ;
         String opcaoUsuario, password;
 
+        // Usuario deve escolher utilizador ADMIN ou CLIENTE
         do {
             System.out.print("Tipo de utilizador (ADMIN ou CLIENTE): ");
             opcaoUsuario = input.nextLine();
 
+                // Se escolher ADMIN, deve informar a password correta
             if (opcaoUsuario.equals("ADMIN")) {
                 System.out.print("Digite a password: ");
                 password = input.nextLine();
 
+                // Se a senha se confirmar, entramos no menu ADMIN
                 if (password.equals("123")) {
+
+                    // Se usuario digitar utilizador ADMIN, e passa pela verificaçao de senha, abre se o menu ADMIN
+                    do {
                     System.out.println("\n\n****** MENU ADMIN ******");
                     System.out.println("1. Imprima o seu conteúdo na consola");
                     System.out.println("2. Imprima quantas vendas foram executadas e o seu valor total");
@@ -31,12 +38,45 @@ public class proj_01 {
                     System.out.println("5. Imprima qual o jogo mais caro e os clientes que o compraram");
                     System.out.println("0. Sair");
                     System.out.print("Insira a opção desejada: ");
-                    int opcaoAdmin = input.nextInt();
+                    opcaoAdmin = input.nextInt();
+
+                        switch (opcaoAdmin) {
+                            case 1: // Imprima o seu conteúdo na consola
+                                System.out.println("\n****** IMPRIMIR CONTEUDO ******");
+                                imprimirFicheiro("Ficheiros/GameStart_V2.csv");
+                                break;
+                            case 2: // Imprima quantas vendas foram executadas e o seu valor total
+                                System.out.println("\n****** VALOR TOTAL DE VENDAS******");
+                                regVendasTot(matrizTotal);
+                                break;
+                            case 3: // Sabendo que a GameStart tem 20% de lucro em cada jogo, calcule o total de lucro.
+                                System.out.println("\n****** VALOR TOTAL DE VENDAS + 20% LUCRO ******");
+                                VendasTotLucro(matrizTotal);
+                                break;
+                            case 4: // Dado um idCliente, imprima todas as informações associadas a esse cliente (nome, contacto, email).
+                                System.out.println("\n****** DADOS CLIENTE******");
+                                idClientesFicheiro(matrizTotal);
+                                break;
+                            case 5: // Imprima qual o jogo mais caro e os clientes que o compraram
+                                System.out.println("\n****** JOGO MAIS CARO VENDIDO ******");
+                                jogoMaisCaro(matrizTotal);
+                                break;
+                            case 0:
+                                System.out.println("\n<><><> Até logo <><><>");
+                                break;
+                            default:
+                                System.out.println("\nOpção inválida!");
+                        }
+
+                        // Menu encerra-se quando o usuario digitar 0
+                    } while (opcaoAdmin != 0);
+
+                    // caso a senha seja incorreta, o programa se encerra
                 } else {
                     System.out.println("Senha invalida!");
                 }
 
-
+                // Se usuario digitar utilizador CLIENTE, abre se o menu CLIENTE
             } else if (opcaoUsuario.equals("CLIENTE")) {
 
                 do {
@@ -50,44 +90,34 @@ public class proj_01 {
                     opcaoCliente = input.nextInt();
 
                     switch (opcaoCliente) {
-                        case 1:
+                        case 1: // Registar cliente
                             System.out.println("\n****** REGISTO DE CLIENTE ******");
                             registoCliente(input);
                             break;
-
-                        case 2:
+                        case 2: // Procurar estacionamento vago
                             System.out.println("\n****** ESTACIONAMENTO ******");
                             listarVagas(matrizTotal);
                             break;
-
-                        case 3:
+                        case 3: // Listar jogos do ficheiro
                             System.out.println("\n****** LISTA DE JOGOS ******");
                             listarJogosFicheiro(matrizTotal);
                             break;
-
-                        case 4:
+                        case 4: // listar jogos por categoria do ficheiro e imprmir esteticamente
                             System.out.println("\n****** JOGOS POR CATEGORIA ******");
-                            System.out.print("Editora a pesquisar: ");
-                            input.nextLine();
-                            String editora = input.nextLine();
-                            for (int linha = 0; linha < matrizTotal.length; linha++) {
-                                if (matrizTotal[linha][5].equals(editora)) {
-                                    System.out.println("Categoria: " + matrizTotal[linha][6] +  " - " + matrizTotal[linha][7]);
-                                }
-                            }
+                            jogosPorcategoria(matrizTotal);
                             break;
-
                         case 0:
                             System.out.println("\n<><><> Até logo <><><>");
                             break;
-
                         default:
                             System.out.println("\nOpção inválida!");
                     }
 
+                    // MMenu encerra-se quando o usuario digitar 0
                 } while (opcaoCliente != 0);
-
                 break;
+
+                // Se usuario digitar utiliador invalido, o programa se encerra
             } else {
                 System.out.println("Tipo de usuario inválido!");
                 break;
