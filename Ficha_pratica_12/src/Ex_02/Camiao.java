0fcda84c0160d27398088668ce418d64ed70133a
Ricardo Fernandes <143021198+ricardolagarini@users.noreply.github.com>
2023-11-27 16:55:53 +0000

    2
    2

--- Ficha_pratica_12/src/Ex_02/Camiao.java
+++ Ficha_pratica_12/src/Ex_02/Camiao.java
@@ -1,4 +1,28 @@
 package Ex_02;
 
-public class Camiao {
+import Ex_02.Enums.TipoCombustivel;
+
+public class Camiao extends Veiculo {
+
+    double capacidaCarga;
+
+
+    public Camiao(String marca, String modelo, int anoFabrico, int potencia, int cilindrada, TipoCombustivel tipoCombustivel, double consumo, double capacidaCarga) {
+        super(marca, modelo, anoFabrico, potencia, cilindrada, tipoCombustivel, consumo);
+        this.capacidaCarga = capacidaCarga;
+    }
+
+
+    public void calcularCarga(double cargaExtra, double distancia) {
+        if(this.capacidaCarga>cargaExtra){
+            double capAtual=this.capacidaCarga;
+            capAtual+=cargaExtra;
+
+        }
+
+
+    }
+
+
+
 }
--- Ficha_pratica_12/src/Ex_02/Main.java
+++ Ficha_pratica_12/src/Ex_02/Main.java
@@ -6,12 +6,15 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Carro carro01 = new Carro("BMW","C220",2019,200,1000, TipoCombustivel.GASOLINA,5,5);
+        Carro carro01 = new Carro("BMW","C220",2019,200,1000, TipoCombustivel.GASOLINA,15,5);
+        Camiao camiao01 = new Camiao("volvo","V1000",2020,800,3000,TipoCombustivel.DIESEL,30,4000);
+
+
 
         //consumo 5l s cada 100km
 
 
-        carro01.calcularCusto(40);
+        carro01.calcularCusto(50);
 
 
 
